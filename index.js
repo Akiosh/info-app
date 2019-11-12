823c37ba79d63310c542bd4e37492952506c2914
aliaksandr_prakapovich <wherearemybigdonuts@gmail.com>
2019-11-12 19:58:03 +0300

    added todo logic

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const express = require('express');
+var cors = require('cors');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const toDos = ['asd', 'asd', 'asdasds'];
 
 express()
+  .use(express.json())
+  .use(cors())
   .get('/', (req, res) => res.send('<h1> some info <h1/>'))
-  .get('/data', (req, res) => res.json({some: 'info'}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/todos', (req, res) => {
+    res.json(toDos);
+  })
+  .post('/todos', (req, res) => { 
+    toDos.push(req.body.todoElem);
+    res.json(toDos);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
