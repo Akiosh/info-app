e143f8d958b2f673d19e0d25510abbfffcc0a3f4
aliaksandr_prakapovich <wherearemybigdonuts@gmail.com>
2019-12-07 18:12:39 +0300

    add static files

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 var cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
-let todos =[{"value":"aSDA","checked":false}];
+let todos = [{ "value": "aSDA", "checked": false }];
 
 express()
   .use(express.json())
   .use(cors())
-  .get('/', (req, res) => res.send('<h1> some info <h1/>'))
+  // .get('/info', (req, res) => res.send('<h1> hello world <h1/>'))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+  .get('/cat', (req, res) => res.sendFile(path.join(__dirname + '/view/cat.png')))
   .get('/todos', (req, res) => {
+    console.log('get todos ->>', todos);
     res.json(todos);
   })
   .post('/todos', (req, res) => { 
-    console.log(req.body);
+    console.log('post req.body ->>', req.body);
     todos = req.body;
     console.log('hi')
     res.json(todos);
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use('/view', express.static('view'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`)); // listening http://localhost:5000/
